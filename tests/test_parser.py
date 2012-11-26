d0db155dc69f8eb315a3b3930055dbaf51ba2859
Marco Plaisier <marco@finalist.com>
2012-11-26 21:53:13 +0100

    improved tests for parser
    Signed-off-by: Marco Plaisier <marco@finalist.com>

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,27 +1,39 @@
 import unittest
 import os
-from parser.parser import BuienRadarParser
+from weathervane.parser.parser import BuienradarParser
+from datetime import datetime
 
 class ParserTestCase(unittest.TestCase):
     def setUp(self):
-        file_location = os.path.join(os.getcwd(), "tests", "data.txt")
-        response = open(file_location)
-        self.parser = BuienRadarParser(response)
+        pass
 
-    def test_station_codes(self):
-        station_codes = self.parser.get_station_codes()
-        expected_codes = [u'6391', u'6275', u'6249', u'6308', u'6260', 
-                          u'6235', u'6370', u'6377', u'6321', u'6350', 
-                          u'6323', u'6283', u'6280', u'6315', u'6278',
-                          u'6356', u'6330', u'6311', u'6279', u'6251',
-                          u'6258', u'6285', u'6209', u'6225', u'6210', 
-                          u'6277', u'6320', u'6270', u'6269', u'6348', 
-                          u'6380', u'6273', u'6286', u'6312', u'6344', 
-                          u'6343', u'6316', u'6240', u'6324', u'6267', 
-                          u'6229', u'6331', u'6290', u'6313', u'6242', 
-                          u'6310', u'6375', u'6319', u'6248', u'6257', 
-                          u'6340', u'6239', u'6252']
-        self.assertEqual(station_codes, expected_codes)
+    def test_get_station_name(self):
+        testdata_excerpt = '''<weerstation id="6323">
+                                  <stationnaam regio="Goes">Meetstation Goes</stationnaam>
+                              </weerstation>'''
+        parser = BuienradarParser(testdata_excerpt)
+        expected = 'Meetstation Goes'
+        result = parser.get_station_name_by_id(6323)
+        self.assertEqual(expected, result, "Expected station name %s does not match %s" % (expected, result))
+
+    def test_get_date(self):
+        testdata_excerpt = '''<weerstation id="6323">
+                                  <datum>11/06/2012 21:40:00</datum>
+                              </weerstation>'''
+        parser = BuienradarParser(testdata_excerpt)
+        expected = datetime(2012, 6, 11, 21, 40, 00)
+        result = parser.get_date(6323)
+        self.assertEqual(result, expected, 'Expected date %s does not match result %s' % (expected, result))
+
+    def test_get_wind_direction(self):
+        test_data_excerpt = '''<weerstation id="6323"><stationcode>6323</stationcode>
+                                   <stationnaam regio="Goes">Meetstation Goes</stationnaam>
+                                   <windsnelheidMS>9.01</windsnelheidMS>
+                               </weerstation>'''
+        parser = BuienradarParser(test_data_excerpt)
+        expected = 9.01
+        result = parser.get_wind_speed(6323)
+        self.assertEqual(result, expected, 'Expected windspeed %s does not match result %s' % (expected, result))
 
 if __name__ == "__main__":
     unittest.main()
--- weathervane/parser/parser.py
+++ weathervane/parser/parser.py
@@ -1,32 +1,41 @@
 from BeautifulSoup import BeautifulSoup
 from urllib2 import urlopen
+import datetime
 import re
 
-class BuienRadarParser:
+class BuienradarParser(object):
     def __init__(self, data):
         self.soup = BeautifulSoup(data)
 
+    def get_date(self, station_id):
+        date = self.get_data_from_station(station_id, 'datum')
+        return datetime.datetime.strptime(date, '%d/%m/%Y %H:%M:%S')
+
     def get_data_from_station(self, station_id, fieldname):
         station_data = self.soup.find("weerstation", id=station_id).find(fieldname)
-        return station_data 
+        return station_data.string
 
     def get_station_codes(self):
-        codetags = self.soup("stationcode")
-        codes = [tag.string for tag in codetags]
+        code_tags = self.soup("stationcode")
+        codes = [tag.string for tag in code_tags]
         return codes
 
-    def get_station_name_by_id(self, number):
-        station_name = self.get_data_from_station(number, "stationnaam")
+    def get_station_name_by_id(self, station_id):
+        station_name = self.get_data_from_station(station_id, "stationnaam")
         return station_name
 
-    def get_station_windrichting(self, number):
-        windrichting = self.get_data_from_station(number, "windrichtinggr")
-        return windrichting
+    def get_station_wind_direction(self, station_id):
+        wind_direction = self.get_data_from_station(station_id, "windrichtinggr")
+        return int(wind_direction)
+
+    def get_wind_speed(self, station_id):
+        wind_speed = self.get_data_from_station(station_id, "windsnelheidms")
+        return float(wind_speed)
 
 if __name__ == "__main__":
     response = urlopen("http://xml.buienradar.nl")
     data = response.read()
-    wb = BuienRadarParser(data)
+    wb = BuienradarParser(data)
     codes = wb.get_station_codes()
     print codes
     name = wb.get_station_name_by_id(id)

434f9c67e41ef789446690235a729f5f01e8f53f
Marco Plaisier <marco@finalist.com>
2012-12-10 20:41:07 +0100

    Add interface to SPI

--- weathervane/interfaces/spi.py
+++ weathervane/interfaces/spi.py
@@ -2,10 +2,10 @@ from ctypes import cdll, c_ubyte
 
 __author__ = 'marco'
 
-class SetupException(Exception):
+class SPISetupException(Exception):
     pass
 
-class DataTransmissionError(Exception):
+class SPIDataTransmissionError(Exception):
     pass
 
 class spi(object):
@@ -13,14 +13,19 @@ class spi(object):
         lib_name = 'wiringPi'
         self.handle = cdll.LoadLibrary(lib_name)
 
-    def setup(channel=0, frequency=500000):
+    def __repr__(self):
+        return
+    def setup(self, channel=0, frequency=500000):
         return_code = self.handle.wiringPiSPISetup(channel, frequency)
         if return_code < -1:
-            raise SetupException('Could not setup SPI protocol.')
+            raise SPISetupException('Could not setup SPI protocol.')
 
-    def send_data(data):
+    def send_data(self, data):
         data_list = c_ubyte*len(data)
         return_code = self.handle.wiringPiSPIDataRW(0, data_list, len(data))
         if return_code < -1:
-            raise DataTransmissionError('Problem with transmission')
-        return data_list
+            raise SPIDataTransmissionError('Problem with transmission')
+        self.data_list = data_list
+
+    def get_data(self):
+        return self.data_list

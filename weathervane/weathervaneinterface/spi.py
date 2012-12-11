3c2a44c1c9e542e31fcc8f61d5ecad06c813ca30
Marco Plaisier <marco@finalist.com>
2012-12-11 21:30:25 +0100

    Revert "Add interface to SPI"
    This reverts commit 434f9c67e41ef789446690235a729f5f01e8f53f.

--- weathervane/weathervaneinterface/spi.py
+++ weathervane/weathervaneinterface/spi.py
@@ -2,10 +2,10 @@ from ctypes import cdll, c_ubyte
 
 __author__ = 'marco'
 
-class SPISetupException(Exception):
+class SetupException(Exception):
     pass
 
-class SPIDataTransmissionError(Exception):
+class DataTransmissionError(Exception):
     pass
 
 class spi(object):
@@ -13,19 +13,14 @@ class spi(object):
         lib_name = 'wiringPi'
         self.handle = cdll.LoadLibrary(lib_name)
 
-    def __repr__(self):
-        return
-    def setup(self, channel=0, frequency=500000):
+    def setup(channel=0, frequency=500000):
         return_code = self.handle.wiringPiSPISetup(channel, frequency)
         if return_code < -1:
-            raise SPISetupException('Could not setup SPI protocol.')
+            raise SetupException('Could not setup SPI protocol.')
 
-    def send_data(self, data):
+    def send_data(data):
         data_list = c_ubyte*len(data)
         return_code = self.handle.wiringPiSPIDataRW(0, data_list, len(data))
         if return_code < -1:
-            raise SPIDataTransmissionError('Problem with transmission')
-        self.data_list = data_list
-
-    def get_data(self):
-        return self.data_list
+            raise DataTransmissionError('Problem with transmission')
+        return data_list

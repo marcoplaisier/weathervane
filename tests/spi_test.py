0a6459a5fbaf3533f1d33401099521a394f1a954
Marco Plaisier <marco.plaisier@finalist.com>
2014-02-03 21:44:19 +0100

    Add a few tests for SPI.py

--- tests/spi_test.py
+++ tests/spi_test.py
@@ -1,9 +1,23 @@
+from mock import patch
+from weathervane.interfaces.SPI import SPI, SPISetupException
+
 __author__ = 'Marco'
 
 import unittest
 
+
 class MyTestCase(unittest.TestCase):
-    pass
+    def test_initialization_wrong_channel(self):
+        self.assertRaises(SPISetupException, SPI, channel=2)
+
+    def test_initialization_wrong_lib(self):
+        self.assertRaises(SPISetupException, SPI, library='not-existing-here')
+
+    def test_incorrect_return_code(self):
+        pass
+        #spi = SPI(channel=0, frequency=500000)
+        #TODO: add tests
+
 
 if __name__ == '__main__':
     unittest.main()

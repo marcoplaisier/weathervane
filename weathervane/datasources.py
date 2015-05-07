0d30bec8f5cd346316b48e9b6ce8ecb15c642fc4
Marco Plaisier <marco.plaisier@finalist.com>
2015-05-07 07:34:12 +0200

    Remove unneeded self parameters

--- weathervane/datasources.py
+++ weathervane/datasources.py
@@ -2,13 +2,13 @@ from urllib2 import urlopen
 from parser import BuienradarParser
 
 
-def retrieve_xml(self, url):
+def retrieve_xml(url):
     response = urlopen(url)
     data = response.read()
     return data
 
 
-def fetch_weather_data(self, conn, station_id, *args, **kwargs):
+def fetch_weather_data(conn, station_id, *args, **kwargs):
     data = retrieve_xml("http://xml.buienradar.nl")
     wd = BuienradarParser.parse(data, *args, **kwargs)
 

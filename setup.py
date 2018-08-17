0d9008322f6ecba0830e1456d5a6cc073ecfb1a8
Marco Plaisier <m.plaisier@gmail.com>
2018-08-17 15:04:32 +0200

    Create installer

--- setup.py
+++ setup.py
@@ -1,26 +1,43 @@
-from setuptools import setup
+import os
+import sys
+
+from setuptools import setup, find_packages
+from setuptools.command.install import install
 
 
-def readme():
+def long_description():
     with open('README.rst') as f:
         return f.read()
 
 
+class Install(install):
+    def run(self):
+        operating_system = os.environ['OS']
+        if 'windows' in operating_system.lower():
+            sys.exit('Cannot install on Windows')
+        print('Installing WiringPi...')
+        if not os.system('gpio -v'):
+            os.system('git clone git://git.drogon.net/wiringPi')
+            os.system('./wiringPi/build')
+        else:
+            print('WiringPi already installed; skipping.')
+
+
 setup(name='weathervane',
-      version='0.2',
+      version='0.3',
       description='Collect weather data from public source(s) and display on 32-LED weathervane',
-      url='http://github.com/marcofinalist/weathervane',
+      long_description=long_description(),
+      long_description_content_type='text/x-rst',
+      url='http://github.com/marcoplaisier/weathervane',
       author='Marco Plaisier',
       author_email='m.plaisier@gmail.com',
       license='MIT',
-      packages=['weathervane'],
+      packages=find_packages(),
       zip_safe=False,
       classifiers=[
-          'Development Status :: 2 - Pre-Alpha',
           'Natural Language :: English',
-          'Operating System :: Linux',
-          'Programming Language :: Python :: 2.7',
-          'Topic :: Home Automation',
+          'Programming Language :: Python :: 3',
+          'Topic :: Home Automation'
       ],
-      requires=['BeautifulSoup', 'mock']
+      cmdclass={'install': Install}
       )

992be506b3e0029a0777249e8bf15fb1f817f048
Marco Plaisier <mplaisier@bol.com>
2021-06-19 13:50:12 +0200

    weathervane-43 Change tests to new fields

--- tests/test_weathervaneConfigParser.py
+++ tests/test_weathervaneConfigParser.py
@@ -7,8 +7,6 @@ from weathervane.parser import WeathervaneConfigParser
 
 
 class TestWeathervaneConfigParser(TestCase):
-    NUMBER_OF_EXPECTED_BITS = 14
-
     def test_parse_config(self):
         config_file = os.path.join(os.getcwd(), 'tests', 'config-test1.ini')
         cp = WeathervaneConfigParser()
@@ -17,8 +15,7 @@ class TestWeathervaneConfigParser(TestCase):
         expected_keys = ['extended-error-mode', 'channel', 'frequency', 'library', 'interval', 'source',
                          'stations', 'bits', 'sleep-time', 'test', 'barometric_trend', 'display']
         self.assertEqual(set(observed.keys()), set(expected_keys))
-        expected_bits = {str(x): x for x in range(self.NUMBER_OF_EXPECTED_BITS + 1)}
-        self.assertEqual(set(expected_bits.keys()), set(observed['bits'].keys()))
+        self.assertEqual(16, len(observed['bits']))
 
     def test_parse_station_numbers(self):
         config_file = os.path.join(os.getcwd(), 'tests', 'config-test1.ini')
@@ -28,14 +25,14 @@ class TestWeathervaneConfigParser(TestCase):
         assert stations == [6320, 6308]
 
     def test_rain_config(self):
-        'Tests if rain configuration is correctly picked up. E.g. bit_13=rain_mm_per_hour,10,0,99.9,0.1'
+        'Tests if rain configuration is correctly picked up. E.g. bit_13=rainFallLastHour,10,0,99.9,0.1'
         config_file = os.path.join(os.getcwd(), 'tests', 'config-test1.ini')
         cp = WeathervaneConfigParser()
         cp.read(config_file)
         observed = cp.parse_config()
-        rain_config = observed['bits']['13']
+        rain_config = observed['bits'][13]
         assert rain_config == {
-            'key': 'rain_mm_per_hour',
+            'key': 'rainFallLastHour',
             'length': '10',
             'min': '0',
             'max': '99.9',

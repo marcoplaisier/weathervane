bccbd5616588c0a46249578327fc8e25b63193cf
Marco Plaisier <mplaisier@bol.com>
2022-08-30 20:53:09 +0200

    Forgot to convert from string to datetime object

--- weathervane/parser.py
+++ weathervane/parser.py
@@ -116,7 +116,8 @@ class BuienradarParser(object):
     def enrich(weather_data: dict) -> dict:
         weather_data['barometric_trend'] = 4
 
-        time_delta = abs(datetime.now() - weather_data['timestamp'])
+        weather_data_timestamp = datetime.fromisoformat(weather_data['timestamp'])
+        time_delta = abs(datetime.now() - weather_data_timestamp)
         if time_delta > timedelta(hours=2):
             logging.warning(f"{weather_data['timestamp']} is more than {time_delta.hours} hours out of date")
             weather_data['error'] = True

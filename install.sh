2e212813278614df304ff9170ae7aeecb444dc31
Marco Plaisier <m.plaisier@gmail.com>
2024-02-09 20:23:50 +0100

    Fix broken git install

--- install.sh
+++ install.sh
@@ -41,7 +41,7 @@ echo "Checking Git installation..."
 has_git=$(git --version) >/dev/null 2>&1
 if [ ! "$has_git" ]; then
   echo "Git not found; installing Git..."
-  apt-get install git -y
+  apt-get install git git-man libc6:armhf liberror-perl -y
   echo "Git installed."
 fi
 echo "Git done"
